e3cdb441f36c138a9fa7dff5797a95ae9f425924
Stevie Siy <stevie.siy@gmail.com>
2023-01-16 22:19:04 -0600

    Executable Created.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    console=['test_py2exe_odbc.py'],
+    options={
+        'py2exe':{
+            'packages':['pandas', 'pyodbc']
+        }
+    }    
+    )
