cbca4f0e233a523fbb705da3af5039a9e02e0730
Stevie <smsiy@catchintelligence.com>
2023-01-20 00:14:01 -0600

    Included comments in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+# import necessary python packages
 from distutils.core import setup
 import py2exe
 
+# setup is the method called via bash commands: 
+# python setup.py install
+# python setup.py py2exe
 setup(
+    # console provides the location of the python script to be converted
     console=['test_py2exe_odbc.py'],
+    
+    # options is where there are customization options
     options={
+        # specifications of the program that is to be compiled
         'py2exe':{
+            # list of packages to include with subpackages
             'packages':['pandas', 'pyodbc'],
+            # directory in which to build the final files
             'dist_dir': 'py2exeODBCv1'
         }
     }    
